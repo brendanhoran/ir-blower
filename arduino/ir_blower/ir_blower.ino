3f1809f37b40e31196103f6bc370fb8f2e8fc982
Brendan Horan <brendan@horan.hk>
2017-05-24 20:38:30 +0800

    Update arduino

--- arduino/ir_blower/ir_blower.ino
+++ arduino/ir_blower/ir_blower.ino
@@ -1,50 +1,52 @@
+// Author  : Brendan Horan
 // License : BSD 3-Clause
-// Version 0.0.1
-// Author : Brendan Horan
+// Version : 0.0.1
+// NOTES   :
+//  *  Install a 10uf capacitor between RESET and GND.
+//     This prevents the Arduino Nano from restarting
+//     on serial port open.
+//  *  Physical pin 3 -> IR sender
+//  *  Physical pin 4 -> power control
 //
-// NOTES :
-//   Install a 10uf capacitor between RESET and GND.
-//   This prevents the Arduino Nano from restarting
-//   on serial port open.
-
+// Requires the IRremote library :
+// https://github.com/shirriff/Arduino-IRremote
+// NOTE some libraries are licensed under other licenses
+​
+​
 #include <IRremote.h>
 IRsend irsend;
-
+​
 // Variable to hold incoming serial comms
 int ctlmsg = 0;   
-
+​
+// Define Power controler pin
+int pctl = 4;
+​
+​
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-        Serial.print ("Connected:9600");
+        pinMode(pctl,OUTPUT);
+        digitalWrite(pctl,HIGH);
+        Serial.begin(9600);     
+        Serial.println ("Connected:9600");
+        Serial.println ("OK");
 }
-
+​
+​
 void loop() {
-
+  
+​
         // wait for data
         if (Serial.available() > 0) {
                 // read data 
                 ctlmsg = Serial.read();
-
-                // send IR signal based on value from ir-blower server
-               // 49 =1 , 50=2 etc
+                Serial.print (ctlmsg);
+                
+​
+               // send IR signal based on value from ir-blower server
+               // IR-blower-server will send value (1-6, a-f)
+               // Arduino client reads the ASCII value and acts on it
+               
                 switch (ctlmsg) {
                   case 49:
-                    Serial.print("49");
-                  //  irsend.send
-                    break;
-                  case 50:
-                    Serial.print("50");
-                 //   irsend.send
-                    break;
-                  case 51:
-                    Serial.print("51");
-                 //   irsend.send
-                    break;
-                  case 52:
-                    Serial.print("52");
-                 //   irsend.send
-                    break;
-                }
-               
-        }
-}
+                    // Value 1 
+                    // Master 9 input select +
